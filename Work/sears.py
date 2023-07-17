33baa1333dbbcba3c7eb11442229a3145f14fb68
ryan <ryangyan@163.com>
2023-07-17 17:20:38 +0800

    format code

--- Work/sears.py
+++ Work/sears.py
@@ -1,7 +1,7 @@
-bill_thickness = 0.11 * 0.001    # Meters (0.11 mm)
-sears_height   = 442             # Height (meters)
-num_bills      = 1
-days            = 1
+bill_thickness = 0.11 * 0.001  # Meters (0.11 mm)
+sears_height = 442  # Height (meters)
+num_bills = 1
+days = 1
 
 while num_bills * bill_thickness < sears_height:
     print(days, num_bills, num_bills * bill_thickness)
