4a0bc0bbcca081c7d0f66d39933718dc4ef558dd
ryan <ryangyan@163.com>
2023-07-17 18:35:24 +0800

    new solution

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,26 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while principal > 0:
+    month = month + 1
+    principal = principal * (1 + rate / 12) - payment
+    total_paid = total_paid + payment
+    # so the extra pay will not impact the interest
+    if extra_payment_start_month <= month <= extra_payment_end_month:
+        principal -= extra_payment
+        total_paid += extra_payment
+
+    print(month, round(total_paid, 2), round(principal, 2))
+
+print('Total month', month)
+print('Total paid', total_paid)
